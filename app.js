bc9b443e0c0008b2dc91090a45e5ca0543095e5a
terkrub <terkrub9@gmail.com>
2022-09-09 14:25:03 +1200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const cors = require('cors')
 const scoketIO = require('socket.io')
+const { isGeneratorObject } = require('util/types')
 
 const app = express()
 const server = http.createServer(app);
@@ -15,15 +16,26 @@ server.listen(PORT,()=>{
 })
 
 io.on("connection",(socket)=>{
-    console.log("New user connected",socket.id);
-
-    socket.on("chat message",(msg)=>{
-        console.log(msg)
-        io.emit("chat message",msg)
-    })
+    console.log("New user connected",socket.id)
+    chatID = socket.handshake.query.chatID
+    console.log(chatID)
+    socket.join(chatID)
 
     socket.on("disconnect",()=>{
         console.log("User disconnect")
+        socket.leave(chatID)
     })
+
+    socket.on("chat message",(msg)=>{
+        msgR = JSON.parse(msg)
+        receiverUsername = msgR.receiverUsername
+        senderUsername = msgR.senderUsername
+        text = msgR.text
+
+        console.log(msgR.receiverUsername)
+        io.to(msgR.receiverUsername).emit("chat message",msg)
+    })
+
+    
     
 })
